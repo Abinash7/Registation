94a436d3ef4d44ad7e39371d40b3ae2f0b0a1f27
unknown <abibhandari7@gmail.com>
2020-01-06 16:42:14 +0545

    registration commit

---
+++ LoginandRegistration/Controllers/UserController.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Web;
+using System.Web.Mvc;
+using LoginandRegistration.Models;
+
+
+namespace LoginandRegistration.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        [HttpGet]
+        public ActionResult Registration()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken] 
+        public ActionResult Registration([Bind(Exclude = "IsEmailVerified,ActivationCode")] tblUser user)
+        {
+            bool Status = false;
+            string Message = "";
+            //Validation
+            if (ModelState.IsValid)
+            {
+                //Check User Exists or Not
+                var isExist = IsEmailExist(user.Email);
+                if (isExist)
+                {
+                    ModelState.AddModelError("EmailExist", "Email Already Exists");
+                    return View(user);
+                }
+                //Activation Code
+                user.ActivationCode = Guid.NewGuid();
+
+                //Password Hashing
+                user.Password = Crypto.Hash(user.Password);
+                user.ConfirmPassword = Crypto.Hash(user.ConfirmPassword);
+                user.IsEmailVerified = false;
+                using (basicEntities db = new basicEntities())
+                {
+                    db.tblUsers.Add(user);
+                    db.SaveChanges();
+
+                    //Sending Verification Link
+                    SendVerificationLinkEmail(user.Email, user.ActivationCode.ToString());
+                    Message= "Registration successfully done. Account activation link " +
+                " has been sent to your email id:" + user.Email;
+                    Status = true;
+                }
+            }
+            else
+            {
+                Message = "Invalid Request";
+            }
+            ViewBag.Status = Status;
+            ViewBag.Message = Message;
+            return View(user);
+        }
+
+        [NonAction]
+        public bool IsEmailExist(string email)
+        {
+            using (basicEntities db = new basicEntities())
+            {
+                var v = db.tblUsers.Where(a => a.Email == email).FirstOrDefault();
+                return v != null;
+            }
+        }
+
+        [NonAction]
+        public void SendVerificationLinkEmail(string email, string activation)
+        {
+            var verifyUrl = "/User/VerifyAccount" + activation;
+            var link = Request.Url.AbsoluteUri.Replace(Request.Url.PathAndQuery, verifyUrl);
+
+            var fromEmail = new MailAddress("abibhandari7@gmail.com", "Abinash Bhandari");
+            var toEmail = new MailAddress(email);
+            var fromEmailPassword = "abinash727";
+
+            string subject = "Your Account Has Been Created Successfully!!!";
+            string body = "<br/>We are excited to tell you that your Dotnet Awesome account is" +
+                " successfully created. Please click on the below link to verify your account" +
+                " <br/><br/><a href='" + link + "'>" + link + "</a> ";
+
+            var SMTP = new SmtpClient
+            {
+                Host = "smtp.gmail.com",
+                Port = 587,
+                EnableSsl = true,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential(fromEmail.Address, fromEmailPassword)
+            };
+
+            using (var Message = new MailMessage(fromEmail, toEmail)
+            {
+                Subject = subject,
+                Body = body,
+                IsBodyHtml = true
+            })
+            SMTP.Send(Message);
+        }
+    }
+}
---
+++ LoginandRegistration/Models/Extended/tblUser.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace LoginandRegistration.Models
+{
+    [MetadataType(typeof(UserMetaData))]
+    public partial class tblUser
+    {
+        public string ConfirmPassword { get; set; }
+    }
+    public class UserMetaData
+    {
+        [Required(AllowEmptyStrings =false,ErrorMessage ="First Name Required")]
+        [Display(Name ="First Name")]
+        public string FirstName { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Last Name Required")]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Email Required")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Display(Name ="Date Of Birth")]
+        [DataType(DataType.Date)]
+        public Nullable<System.DateTime> DOB { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Password Required")]
+        [DataType(DataType.Password)]
+        [MinLength(8,ErrorMessage ="Minimum 8 characters required")]       
+        public string Password { get; set; }
+
+        [Display(Name ="Confirm Password")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Password and Confirm Password doesn't match")]
+        public string ConfirmPassword { get; set; }
+    }
+
+}
